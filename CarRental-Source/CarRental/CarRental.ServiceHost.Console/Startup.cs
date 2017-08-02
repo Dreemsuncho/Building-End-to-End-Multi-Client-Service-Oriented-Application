bce47d66940e518543837f43578a9da62d7498ba
Elvis Arabadjiyski <dreemsuncho@gmail.com>
2017-08-02 12:54:23 +0300

    Host Inventory, Rental and Account services by console client, and add some tests for connectivity

---
+++ CarRental-Source/CarRental/CarRental.ServiceHost.Console/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SM = System.ServiceModel;
+
+using CarRental.Business.Managers;
+
+namespace CarRental.ServiceHost
+{
+    class Startup
+    {
+        static void Main()
+        {
+            Console.WriteLine("Starting up services...");
+            Console.WriteLine();
+
+            SM.ServiceHost hostInventoryManager = new SM.ServiceHost(typeof(InventoryManager));
+            SM.ServiceHost hostRentalManager= new SM.ServiceHost(typeof(RentalManager));
+            SM.ServiceHost hostAccountManager = new SM.ServiceHost(typeof(AccountManager));
+
+            StartService(hostInventoryManager, "InventoryManager");
+            StartService(hostRentalManager, "RentalManager");
+            StartService(hostAccountManager, "AccountManager");
+
+            Console.WriteLine();
+            Console.WriteLine("Press [Enter] to exit.");
+            Console.ReadLine();
+
+            StopService(hostInventoryManager, "InventoryManager");
+            StopService(hostRentalManager, "RentalManager");
+            StopService(hostAccountManager, "AccountManager");
+        }
+
+        static void StartService(SM.ServiceHost host, string description)
+        {
+            host.Open();
+            Console.WriteLine("Service {0} started.", description);
+
+            foreach (var endopint in host.Description.Endpoints)
+            {
+                Console.WriteLine("Listing on endpoints:");
+                Console.WriteLine("Address: {0}", endopint.Address.Uri);
+                Console.WriteLine("Bindings: {0}", endopint.Binding.Name);
+                Console.WriteLine("Contracts: {0}", endopint.Contract.ConfigurationName);
+            }
+            Console.WriteLine();
+        }
+
+        static void StopService(SM.ServiceHost host, string description)
+        {
+            host.Close();
+            Console.WriteLine("Service {0} stopped.", description);
+        }
+    }
+}
---
+++ CarRental-Source/CarRental/CarRental.ServiceHost.Tests/ServiceAccessTests.cs
@@ -0,0 +1,47 @@
+﻿using System.ServiceModel;
+
+using NUnit.Framework;
+using CarRental.Business.Contracts;
+
+namespace CarRental.ServiceHost.Tests
+{
+    [TestFixture]
+    public class ServiceAccessTests
+    {
+        [Test]
+        public void test_inventory_manager_as_service()
+        {
+            // Arange
+            var channelFactory = new ChannelFactory<IInventoryService>("");
+
+            var proxy = channelFactory.CreateChannel();
+
+            // Act & Assert
+            (proxy as ICommunicationObject).Open();
+        }
+
+        [Test]
+        public void test_rental_manager_as_service()
+        {
+            // Arange
+            var channelFactory = new ChannelFactory<IRentalService>("");
+
+            var proxy = channelFactory.CreateChannel();
+
+            // Act & Assert
+            (proxy as ICommunicationObject).Open();
+        }
+
+        [Test]
+        public void test_account_manager_as_service()
+        {
+            // Arange
+            var channelFactory = new ChannelFactory<IAccountService>("");
+
+            var proxy = channelFactory.CreateChannel();
+
+            // Act & Assert
+            (proxy as ICommunicationObject).Open();
+        }
+    }
+}
